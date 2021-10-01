782697a883f55db132745df16eef147db4072a18
Matheus Gaviraghi <49107077+gavimat@users.noreply.github.com>
2021-10-01 14:45:05 -0300

    Add files via upload

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -1,29 +1,32 @@
+
 chmod -R 0755 ./crypto-config
-# # Delete existing artifacts
-# rm -rf ./crypto-config
-# rm genesis.block mychannel.tx
-# rm -rf ../../channel-artifacts/*
+# Delete existing artifacts
+rm -rf ./crypto-config
+rm genesis.block mychannel.tx
+rm -rf ../../channel-artifacts/*
 
 #Generate Crypto artifactes for organizations
-cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
+echo "#######    Generating Crypto materials for organizations under crypto-config folder  ##########"
+/root/fabric-samples/bin/cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
 
-# # System channel
-# SYS_CHANNEL="sys-channel"
+# System channel
+SYS_CHANNEL="sys-channel"
 
-# # channel name defaults to "mychannel"
-# CHANNEL_NAME="mychannel"
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mychannel"
 
-# echo $CHANNEL_NAME
+echo "Channel name: $CHANNEL_NAME"
 
-# # Generate System Genesis block
-# configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
+echo "#######    Generating System Genesis block  ##########"
+# Generate System Genesis block
+/root/fabric-samples/bin/configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
 
+echo "#######    Generating channel configuration block  ##########"
+# Generate channel configuration block
+/root/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
 
-# # Generate channel configuration block
-# configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+echo "#######    Generating anchor peer update for Org1MSP  ##########"
+/root/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 
-# echo "#######    Generating anchor peer update for Org1MSP  ##########"
-# configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
-
-# echo "#######    Generating anchor peer update for Org2MSP  ##########"
-# configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+echo "#######    Generating anchor peer update for Org2MSP  ##########"
+/root/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
